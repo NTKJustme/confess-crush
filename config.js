6708a90b4ffb186739dcbbfc88ce83cc28660ae1
NTKJustme <162573278+NTKJustme@users.noreply.github.com>
2024-03-07 18:29:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Supriseee!!!",
+    introTitle: 'Ebe cute Thân Yến Nhi',
+    introDesc: `Cả khu quân sự có tận 1407 người mà thế nào mình lại va trúng nhau, chắc ko phải do trùng hợp đâu em nhỉ 😁
+    Sau đây anh có vài lời muốn nói với babi của , em sẵn sàng chưa 😁`,
+    btnIntro: '^^Rùi ạ^^',
+    title: 'SUPRISEEE!!!, chắc em cũng phần nào đoán được vì anh gửi vào hôm nay rồi nma',
+    desc: 'Chúc babi 1 ngày 8/3 vui vẻ, chúc em ngày một xinh đẹp, khỏe mạnh và hót hòn họt, hãy cười thật nhiều nhé vì anh rất thích được thấy em cười 😁, chúng mình hãy cùng nhau tạp thật nhiều kỉ niệm như đêm dj ấy nhé 😁',
+    btnYes: 'Em cảm ơn ạ <3<3',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới có vô vàn bông hoa nhưng anh thấy có bông hoa chít toa của anh là đẹp nhất thui',
+    btnReply: 'Gửi cho anh <3',
+    reply: ':))))) dẻo miệng thật đấy',
+    mess: 'Không có chi 😆, vì anh chỉ có mình Thân Yến Nhi, hãy cho anh biết cảm nghĩ của em ',
+    messDesc: '(ko bt coding nên nếu có gì ko hay mong em thông cảm 😁)',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://m.me/nguyen.khoi.23' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
